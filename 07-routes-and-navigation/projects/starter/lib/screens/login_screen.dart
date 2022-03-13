75f59d9f736a1fd9b8775d60d5d7819901040bae
ichn-hu <zfhu16@gmail.com>
2022-03-13 15:22:33 +0800

    ch 07 done'

--- 07-routes-and-navigation/projects/starter/lib/screens/login_screen.dart
+++ 07-routes-and-navigation/projects/starter/lib/screens/login_screen.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:fooderlich/models/app_state_manager.dart';
+import 'package:provider/provider.dart';
+
+import '../models/fooderlich_pages.dart';
 
 class LoginScreen extends StatelessWidget {
-  // TODO: LoginScreen MaterialPage Helper
+  static MaterialPage page() {
+    return MaterialPage(
+      name: FooderlichPages.loginPath,
+      key: ValueKey(FooderlichPages.loginPath),
+      child: const LoginScreen(),
+    );
+  }
 
   final String? username;
 
@@ -59,6 +69,8 @@ class LoginScreen extends StatelessWidget {
         ),
         onPressed: () async {
           // TODO: Login -> Navigate to home
+          Provider.of<AppStateManager>(context, listen: false)
+              .login('username', 'password');
         },
       ),
     );
